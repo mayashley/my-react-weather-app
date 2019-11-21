35ad4a8eeca62664478764ce80f5b6bd9071d8f6
mayashley <54293844+mayashley@users.noreply.github.com>
2019-11-21 14:12:07 -0500

    Updates

---
+++ precache-manifest.3bff7635038ad1e8f3c68818544797ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b0ec98f0211a80c8c0608536e58625",
+    "url": "/my-react-weather-app/index.html"
+  },
+  {
+    "revision": "6430296bbcc96434cd38",
+    "url": "/my-react-weather-app/static/css/main.12551565.chunk.css"
+  },
+  {
+    "revision": "cf7fd05d49d89c3f018a",
+    "url": "/my-react-weather-app/static/js/2.0135ce64.chunk.js"
+  },
+  {
+    "revision": "6430296bbcc96434cd38",
+    "url": "/my-react-weather-app/static/js/main.706f7541.chunk.js"
+  },
+  {
+    "revision": "373b86027750fea8378e",
+    "url": "/my-react-weather-app/static/js/runtime-main.a2508342.js"
+  }
+]);
